e23414839baa5c6a0fb92b747c0d084ed42efa47
clague <lc61@rice.edu>
2019-09-21 13:40:14 -0500

    improving for 2019 app

--- game.py
+++ game.py
@@ -3,112 +3,136 @@ from card import Card
 from player import Player
 from time import sleep
 
+
 class Game:
-	def __init__(self, newPlayers):
-		"""
+    def __init__(self, newPlayers):
+        """
 		takes in a dictionary 'players' which holds <playerName, buy_in> as key, val pairs
 		Has parameters:
 			players
 			dealer
 			deck
 		"""
-		self.players = newPlayers
-		self.dealer = Player("dealer", 99999999999) #has virtually unlimited money
-		self.deck = Deck()
+        self.players = newPlayers
+        self.dealer = Player("dealer", 999999999999999)  # has virtually unlimited money
+        self.deck = Deck()
 
-	def __str__(self):
-		return str(self.players, self.dealer, self.deck)
+    def __str__(self):
+        return str(self.players, self.dealer, self.deck)
 
-	def theBet(self, playersList):
-		"""
+    def theBet(self, playersList):
+        """
 		Just performs theBet portion of the game, locking in everyone's bet
 		"""
-		print("First, the Bet.")
-		wagers = {}
-		for player in playersList:
-			bet = input("How much does " + player.getName() + " want to bet for this round? >") #add some input parsing
-			wagers[player] = int(bet)
-		return wagers
+        print("\033[94m" + "First, the Bet." + "\033[0m")
+        wagers = {}
+        for player in playersList:
+            bet = input(
+                "How much does " + player.getName() + " want to bet for this round? >"
+            )  # add some input parsing
+            wagers[player] = int(bet)
+        return wagers
 
-	def theDeal(self, players):
-		"""
+    def theDeal(self, players):
+        """
 		players is a list of all the Player objects
 		"""
-		print("Shuffling the deck...")
-		sleep(.1)
-		self.deck.shuffle()
-		print(".")
-		sleep(.1)
-		print("Dealing...")
-		for player in players:
-			player.hitMe(self.deck.getCard()) #give a player a random card
-			print(player.getName() + " has " + player.getCards()[0].getFace())
-		# self.dealer.hitMe(self.deck.getCard())
-		for player in players:
-			player.hitMe(self.deck.getCard())
-			print(player.getName() + " has " + player.getCards()[1].getFace())
+        print("Shuffling the deck...")
+        sleep(0.1)
+        self.deck.shuffle()
+        print(".")
+        sleep(0.1)
+        print("Dealing...")
+        for player in players:
+            player.hitMe(self.deck.getCard())  # give a player a random card
+            print(player.getName() + " has " + player.getCards()[0].getFace())
+        # self.dealer.hitMe(self.deck.getCard())
+        for player in players:
+            player.hitMe(self.deck.getCard())
+            print(player.getName() + " has " + player.getCards()[1].getFace())
 
-	def thePlay(self, wagers):
-		"""
+    def thePlay(self, wagers):
+        """
 		wagers is a dictionary containing <player: money bet> for that round
 		"""
-		for player in self.players:
-			print("It is " + player.getName() + " turn. ", " You have " + str([x.getFace() for x in player.getCards()]),
-					" for a total of: " + str(player.getScore()))
-			hitMe = input("Hit ? (Y/N) >")
-			bust = False
-			while (hitMe == "Y" or hitMe == "") and not bust:
-				player.hitMe(self.deck.getCard())
-				print([x.getFace() for x in player.getCards()], " for a total of: " + str(player.getScore()))
-				bust = player.isBust()
-				if bust:
-					break
-				hitMe = input("Hit again? (Y/N) >")
-		# implementing the dealer's play!
-		print("The dealer has " + str([x.getFace() for x in self.dealer.getCards()]))
-		sleep(.9) #The time delays make the experience a whole lot better
-		while self.dealer.getScore() < 17:
-			print("...")
-			sleep(1)
-			self.dealer.hitMe(self.deck.getCard())
-			print("The dealer has: " + str([x.getFace() for x in self.dealer.getCards()]), " for a total of: " + str(self.dealer.getScore()))
-		if self.dealer.isBust():
-			print("The dealer went bust!")
-			sleep(.08)
-			for player in self.players:
-				if not player.isBust():
-					print("...")
-					sleep(1)
-					print(player.getName() + " gets " + str(wagers[player]))
-					player.updateMoney(int(wagers[player])) #update the player.money attribute
-				elif player.isBust():
-					print("You went bust as well, so you still lose :(")
-					sleep(.8)
-					print(player.getName() + " lost " + str(wagers[player]))
-					player.updateMoney(-1*int(wagers[player]))
-		else:
-			# print("The dealer has: " + str([x.getFace() for x in self.dealer.getCards()]), " for a total of: " + str(self.dealer.getScore()))
-			for player in self.players:
-				if self.dealer.getScore() < player.getScore() and not player.isBust():
-					print("...")
-					print("You beat the dealer! " + str(player.getScore()) + " > " + str(self.dealer.getScore()))
-					#then payout
-					print(player.getName() + " gets " + str(wagers[player]))
-					player.updateMoney(int(wagers[player]))
-				else:
-					if player.isBust():
-						print("...")
-						print(player.getName() + " went bust!")
-						print(player.getName() + " lost " + str(wagers[player]))
-						player.updateMoney(-1*int(int(wagers[player])))
-						break
-					print("...")
-					print("The dealer beat you :(" + str(player.getScore()) + " < " + str(self.dealer.getScore()))
-					print(player.getName() + " lost " + str(wagers[player]))
-					player.updateMoney(-1*int(int(wagers[player])))
-		print("...")
-		sleep(1.5)
-		print("The round is over.")
-		# print(".")
-		for player in self.players:
-			print(player.getName() + " now has " + str(player.getMoney()))
+        for player in self.players:
+            print(
+                "It is " + player.getName() + " turn. ",
+                " You have " + str([x.getFace() for x in player.getCards()]),
+                " for a total of: " + str(player.getScore()),
+            )
+            hitMe = input("Hit ? (Y/N) >")
+            bust = False
+            while (hitMe == "Y" or hitMe == "") and not bust:
+                player.hitMe(self.deck.getCard())
+                print(
+                    [x.getFace() for x in player.getCards()],
+                    " for a total of: " + str(player.getScore()),
+                )
+                bust = player.isBust()
+                if bust:
+                    break
+                hitMe = input("Hit again? (Y/N) >")
+        # implementing the dealer's play!
+        print("The dealer has " + str([x.getFace() for x in self.dealer.getCards()]))
+        sleep(0.9)  # The time delays make the experience a whole lot better
+        while self.dealer.getScore() < 17:
+            print("...")
+            sleep(1)
+            self.dealer.hitMe(self.deck.getCard())
+            print(
+                "The dealer has: " + str([x.getFace() for x in self.dealer.getCards()]),
+                " for a total of: " + str(self.dealer.getScore()),
+            )
+        if self.dealer.isBust():
+            print("The dealer went bust!")
+            sleep(0.08)
+            for player in self.players:
+                if not player.isBust():
+                    print("...")
+                    sleep(1)
+                    print(player.getName() + " gets " + str(wagers[player]))
+                    player.updateMoney(
+                        int(wagers[player])
+                    )  # update the player.money attribute
+                elif player.isBust():
+                    print("You went bust as well, so you still lose :(")
+                    sleep(0.8)
+                    print(player.getName() + " lost " + str(wagers[player]))
+                    player.updateMoney(-1 * int(wagers[player]))
+        else:
+            # print("The dealer has: " + str([x.getFace() for x in self.dealer.getCards()]), " for a total of: " + str(self.dealer.getScore()))
+            for player in self.players:
+                if self.dealer.getScore() < player.getScore() and not player.isBust():
+                    print("...")
+                    print(
+                        "You beat the dealer! "
+                        + str(player.getScore())
+                        + " > "
+                        + str(self.dealer.getScore())
+                    )
+                    # then payout
+                    print(player.getName() + " gets " + str(wagers[player]))
+                    player.updateMoney(int(wagers[player]))
+                else:
+                    if player.isBust():
+                        print("...")
+                        print(player.getName() + " went bust!")
+                        print(player.getName() + " lost " + str(wagers[player]))
+                        player.updateMoney(-1 * int(int(wagers[player])))
+                        break
+                    print("...")
+                    print(
+                        "The dealer beat you :("
+                        + str(player.getScore())
+                        + " < "
+                        + str(self.dealer.getScore())
+                    )
+                    print(player.getName() + " lost " + str(wagers[player]))
+                    player.updateMoney(-1 * int(int(wagers[player])))
+        print("...")
+        sleep(1.5)
+        print("The round is over.")
+        # print(".")
+        for player in self.players:
+            print(player.getName() + " now has " + str(player.getMoney()))
--- playGame.py
+++ playGame.py
@@ -8,85 +8,97 @@ import re
 
 
 def daRules():
-	"""
+    """
 	Returns the rules of the Game
 	"""
-	# print("hello")
-	print("Each participant attempts to beat the dealer by getting a count as close to 21 as possible, without going over 21.")
-	print("It is up to each individual player if an ace is worth 1 or 11. Face cards are 10 and any other card is its pip value.")
-	print("A bet once paid and collected is never returned. Thus, one key advantage to the dealer is that the player goes first.")
-	print("If the player goes bust, he has already lost his wager, even if the dealer goes bust as well.")
-	print("------------------------------")
-	print("Really, you'll get the hang of it after playing a few rounds!")
-	print("Be SURE to follow the instructions to the T. Don't try anything goofy or the program will fail :(")
-	print("------------------------------")
+    # print("hello")
+    print(
+        "Each participant attempts to beat the dealer by getting a count as close to 21 as possible, without going over 21."
+    )
+    print(
+        "It is up to each individual player if an ace is worth 1 or 11. Face cards are 10 and any other card is its pip value."
+    )
+    print(
+        "A bet once paid and collected is never returned. Thus, one key advantage to the dealer is that the player goes first."
+    )
+    print(
+        "If the player goes bust, he has already lost his wager, even if the dealer goes bust as well."
+    )
+    print("------------------------------")
+    print("Really, you'll get the hang of it after playing a few rounds!")
+    print(
+        "Be SURE to follow the instructions to the T. Don't try anything goofy or the program will fail :("
+    )
+    print("------------------------------")
 
 
 def addingPlayers(keepAdding, playersList):
-	"""
+    """
 	keepAdding is a boolean flag signalling to continue adding players or not!
 	"""
-	if keepAdding:
-		playerName = input("Add a player! Enter a name >")
-		playerBuyIn = input("How much does " + playerName + " want to buy in? >")
-		player = Player(playerName, playerBuyIn)
-		playersList.append(player)
-		sleep(.3)
-		print(".")
-		print(".")
-		print(".")
-		sleep(.3)
-		addMore = input("Add more players ? (Y/N) > ")
-		if addMore == "N" or addMore == "" or addMore == "n":
-			keepAdding = False
-		return addingPlayers(keepAdding, playersList)
-	else:
-		return playersList
+    if keepAdding:
+        playerName = input("Add a player! Enter a name >")
+        playerBuyIn = input("How much does " + playerName + " want to buy in? >")
+        player = Player(playerName, playerBuyIn)
+        playersList.append(player)
+        sleep(0.3)
+        print(".")
+        print(".")
+        print(".")
+        sleep(0.3)
+        addMore = input("Add more players ? (Y/N) > ")
+        if addMore == "N" or addMore == "" or addMore == "n":
+            keepAdding = False
+        return addingPlayers(keepAdding, playersList)
+    else:
+        return playersList
 
 
-def playGame(): #if the input is some random character (i.e. % or something, then display rules())
-	print("Welcome to BlackJack!")
-	rules = input("For rules, type: 'help' Otherwise type 'play': ")
-	if rules == "help":
-		daRules()
-		playGame()
-	elif rules == "play" or rules == "":
-		#continue with the game !
-		playersList = addingPlayers(True, [])
-		print("Let's begin the Game!")
-		#the Bet goes here
-		keepPlaying = True
-		newPlayersList = playersList
-		theGame = Game(newPlayersList)
-		while keepPlaying:
-			wagers = theGame.theBet(newPlayersList)
-			print("The bets are in!")
-			print("Now we begin to Deal")
-			theGame.theDeal(newPlayersList) #cards have been dealt lit
-			theGame.thePlay(wagers)
-			for player in theGame.players:
-				newPlayersList = []
-				print([x.getName() for x in newPlayersList])
-				if player.getMoney() <= 0:
-					print(player.getName() + " ran out of money :(")
-					continue
-				playAgain = input(player.getName() + " want to continue playing? (Y/N) >")
-				if playAgain != "Y":
-					print("Goodbye " + player.getName())
-					# newPlayersList.remove(player)
-				else:
-					player.resetRound()
-					newPlayersList.append(player)
-			if not newPlayersList:
-				keepPlaying = False
-				print("...")
-				sleep(1)
-				print("Thanks for playing! Come give us your money again soon!")
-				return
-		# We've added everyone!
-	else:
-		print("You done goofed :(")
-		return
+def playGame():  # if the input is some random character (i.e. % or something, then display rules())
+    print("Welcome to BlackJack!")
+    rules = input("For rules, type: 'help' Otherwise type 'play': ")
+    if rules == "help":
+        daRules()
+        playGame()
+    elif rules == "play" or rules == "":
+        # continue with the game !
+        playersList = addingPlayers(True, [])
+        print("Let's begin the Game!")
+        # the Bet goes here
+        keepPlaying = True
+        newPlayersList = playersList
+        theGame = Game(newPlayersList)
+        while keepPlaying:
+            wagers = theGame.theBet(newPlayersList)
+            print("The bets are in!")
+            print("Now we begin to Deal")
+            theGame.theDeal(newPlayersList)  # cards have been dealt lit
+            theGame.thePlay(wagers)
+            for player in theGame.players:
+                newPlayersList = []
+                print([x.getName() for x in newPlayersList])
+                if player.getMoney() <= 0:
+                    print(player.getName() + " ran out of money :(")
+                    continue
+                playAgain = input(
+                    player.getName() + " want to continue playing? (Y/N) >"
+                )
+                if playAgain not in ["Y", "y", "yes", "yuh", "yeah"]:
+                    print("Goodbye " + player.getName())
+                else:
+                    player.resetRound()
+                    newPlayersList.append(player)
+            if not newPlayersList:
+                keepPlaying = False
+                print("...")
+                sleep(1)
+                print("Thanks for playing! Come give us your money again soon!")
+                return
+        # We've added everyone!
+    else:
+        print("You done goofed :(")
+        return
+
 
 ### actually drive the game below here
 
